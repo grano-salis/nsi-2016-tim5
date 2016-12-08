525fecceb2456bf0572fd1bd1dce96ba614aecec
Emina Šarić <esaric1@etf.unsa.ba>
2016-12-08 19:09:07 +0100

    "Frontend" za export

--- NSICollection.Web/app/app.js
+++ NSICollection.Web/app/app.js
@@ -1,55 +1,54 @@
-(function(){
+(function () {
 
-  // Declare app level module which depends on views, and components
-  var app = angular.module('echo', ['ngRoute']);
-  
-  app.config(function($routeProvider) {
-        $routeProvider            
-            .when('/', {
-                templateUrl : 'views/home.html',
-                controller  : 'homeCtrl'
-            })
-             .when('/addCollection', {
-                templateUrl : 'views/addCollection.html',
-                controller  : 'Ctrl3'
-            })            
-            .when('/login', {
-                templateUrl : 'views/loginForm.html',
-                controller  : 'loginCtrl'
-            })
+    // Declare app level module which depends on views, and components
+    var app = angular.module('echo', ['ngRoute']);
 
-            .when('/addItem', {
-                templateUrl : 'views/addItem.html',
-                controller  : 'itemCtrl'
-            })
-            .when('/library', {
-                templateUrl: 'views/library.html',
-                controller: 'libraryCtrl'
-            });
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/addCollection', {
+            templateUrl: 'views/addCollection.html',
+            controller: 'Ctrl3'
+        })
+        .when('/login', {
+            templateUrl: 'views/loginForm.html',
+            controller: 'loginCtrl'
+        })
 
-            
+        .when('/addItem', {
+            templateUrl: 'views/addItem.html',
+            controller: 'itemCtrl'
+        })
+        .when('/library', {
+            templateUrl: 'views/library.html',
+            controller: 'libraryCtrl'
+        })
+        .when('/exportItem', {
+            templateUrl: 'views/exportItem.html',
+            controller: 'exportCtrl'
+        });
     });
 
 
-  var serviceBase = "";
+    var serviceBase = "";
 
-  if (window.location.origin.search("localhost") == -1) {
-      serviceBase = window.location.origin + "/api/";
-  } else {
-      serviceBase = 'http://localhost:60824/';
-  }
+    if (window.location.origin.search("localhost") == -1) {
+        serviceBase = window.location.origin + "/api/";
+    } else {
+        serviceBase = 'http://localhost:60824/';
+    }
 
-  app.constant('ngAuthSettings', {
-      apiServiceBaseUri: serviceBase,
-      clientId: 'ngAuthApp'
-  });
+    app.constant('ngAuthSettings', {
+        apiServiceBaseUri: serviceBase,
+        clientId: 'ngAuthApp'
+    });
 
-  app.config(['$httpProvider', function ($httpProvider) {
-      $httpProvider.defaults.useXDomain = true;
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  }
-  ]);
-
-    
-
+    app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+    ]);
 })();
---
+++ NSICollection.Web/app/controllers/exportCtrl.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+var app = angular.module('echo');
+
+app.controller('exportCtrl', function ($scope, $http) {
+    $scope.itemList = [];
+    $scope.message = "";
+    $scope.item = {
+        title: "My Document",
+        currentType: "book",
+        isprivate: false,
+        metadata: {
+            author: "Arnela Tumbul",
+            abstract: "Long story short",
+            publisher: "Sarajevo",
+            language: "en",
+            url: "https://google.com",
+            extra: "Addition",
+            issued: "11.10.2016."
+        }
+    };
+});
+
+function exportItem() {
+    $.ajax({
+        url: "http://localhost:60824/api/citation/GetIEEECitationOfFirstItem",
+        dataType: "json",
+        method: "GET",
+        success: function (data, textStatus, jqXHR) {
+            alert(data);
+        }
+    });
+}
